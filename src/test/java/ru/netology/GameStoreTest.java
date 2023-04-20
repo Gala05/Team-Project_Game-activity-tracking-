894d4254c44c798a01bef63297b053dd9cb65b6d
Galina Mikhaylova <gala6858@mail.ru>
2023-04-20 20:49:10 +0300

    adding initial code

---
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -0,0 +1,18 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameStoreTest {
+
+    @Test
+    public void shouldAddGame() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        assertTrue(store.containsGame(game));
+    }
+
+    // другие ваши тесты
+}
