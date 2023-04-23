39031c2dead208a5694aa7812b7630a243a0726d
Papillon <anil_061293@gmail.com>
2023-04-23 16:38:01 +0300

    AddTest in PlayerTest

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -5,6 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
+    @Test
+    public void testIfGameNotInstall() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Баттл Онлайн", "Стратегия");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.play(game, 5);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            player.play(game1, 5);
+        });
+    }
 
     @Test
     public void shouldSumGenreIfOneGame() {
@@ -37,22 +51,6 @@ public class PlayerTest {
     }
 
     @Test
-    public void shouldMostPlayerByGenreIfNoGenre() {
-        GameStore store = new GameStore();
-        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
-        Game game1 = store.publishGame("Баттл Онлайн", "Аркады");
-
-        Player player = new Player("Petya");
-        player.installGame(game);
-        player.play(game, 3);
-        player.play(game1, 2);
-
-
-        String actual = String.valueOf(player.mostPlayerByGenre("Головоломка"));
-        assertEquals(null, actual);
-    }
-
-    @Test
     public void shouldMostPlayerByGenreIfOneGenre() {
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
@@ -60,11 +58,14 @@ public class PlayerTest {
 
         Player player = new Player("Petya");
         player.installGame(game);
+        player.installGame(game1);
+
         player.play(game, 3);
         player.play(game1, 2);
 
-        String expected = "Нетология Баттл Онлайн";
-        String actual = String.valueOf(player.mostPlayerByGenre("Аркада"));
+        Game expected = game;
+        Game actual = player.mostPlayerByGenre("Аркады");
+
         assertEquals(expected, actual);
     }
 
@@ -76,11 +77,33 @@ public class PlayerTest {
 
         Player player = new Player("Petya");
         player.installGame(game);
+        player.installGame(game1);
+
+        player.play(game, 3);
+        player.play(game1, 4);
+
+        Game expected = game1;
+        Game actual = player.mostPlayerByGenre("Головоломка");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMostPlayerByGenreIfNoGenre() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+
         player.play(game, 3);
         player.play(game1, 2);
 
-        String expected = "Нетология Баттл Онлайн";
-        String actual = String.valueOf(player.mostPlayerByGenre("Аркада"));
-        assertEquals(expected, actual);
+        Game expected = null;
+        Game actual = player.mostPlayerByGenre("Головоломка");
+
+        assertEquals(null, actual);
     }
 }
